eb6571056795cdcb6d867967698eece66cc28be6
kngluu <kngluu@gmail.com>
2025-10-18 06:45:07 +0200

    Create rewards.js

---
+++ rewards.js
@@ -0,0 +1,6 @@
+
+// rewards.js
+document.addEventListener("DOMContentLoaded", () => {
+  const rewardsList = document.getElementById("rewardsList");
+  rewardsList.innerHTML = "<li>Daily Login Bonus - 5 points</li><li>Watched Ad - 10 points</li>";
+});
